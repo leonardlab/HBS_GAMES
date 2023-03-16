67f3ee7581207efea0708725c02e6a0246840a1c
Kathleen Dreyer <kathleendreyer2024@u.northwestern.edu>
2023-03-15 23:06:25 -0500

    update HBS config A file to add topologies; copy to config D (to edit); setup solve_single, solve_experiment (to test); add HBS to set_model; set up solve_single_parameter_set for test

--- src/games/models/HBS.py
+++ src/games/models/HBS.py
@@ -7,7 +7,6 @@ Created on Mon Mar 13 2023
 """
 
 import math
-from typing import Tuple, List
 import numpy as np
 from scipy.integrate import odeint
 from games.plots.plots_training_data import plot_training_data_2d
@@ -20,10 +19,12 @@ class HBS_model:
     """
     def __init__(
         self,
-        parameters: List[float] = [1, 1, 1, 1, 1, 1],
-        inputs: List[float] = None,
-        input_ligand: float = 1000,
+        parameters: list[float] = [1, 1, 1, 1, 1, 1],
+        input_pO2: list[float] = None,
         mechanismID: str = "default",
+        t_normoxia: np.ndarray = np.arange(0, 500, 1),
+        t_hypoxia_exp: np.ndarray = np.array([0, 24, 48, 72, 96, 120]),
+        t_hypoxia_plot: np.ndarray = np.linspace(0,120,31)
     ) -> None:
 
         """Initializes HBS model.
@@ -33,11 +34,8 @@ class HBS_model:
         parameters
             List of floats defining the parameters
 
-        inputs
-            List of floats defining the inputs
-
-        # input_ligand
-        #     Float defining the input ligand concentration
+        input_pO2
+            List of floats defining the input pO2: [pO2_normoxia, pO2_hypoxia]
 
         mechanismID
             a string defining the mechanism identity
@@ -47,95 +45,122 @@ class HBS_model:
         None
 
         """
-        self.state_labels = [
-            "ZF mRNA",
-            "ZF protein",
-            "AD mRNA",
-            "AD protein",
-            "Ligand",
-            "Activator",
-            "Rep RNA",
-            "Rep protein",
-        ]
         self.parameters = parameters
-        self.inputs = inputs
-        self.input_ligand = input_ligand
+        self.pO2_normoxia = input_pO2[0]
+        self.pO2_hypoxia = input_pO2[1]
         self.mechanismID = mechanismID
+        self.t_normoxia = t_normoxia
+        self.t_hypoxia_exp = t_hypoxia_exp
+        self.t_hypoxia_plot = t_hypoxia_plot
+
+        if self.mechanismID == "A":
+            self.state_labels = [
+                'HAFR', 'HAFP', 'aHIF', 'HIF1R', 'HIF1P', 
+                'HIF2R', 'HIF2P', 'HIF2P*', 'DSRE2R', 'DSRE2P'
+            ]
+            # self.topology_gradient = self.topology_gradient_A
+        
+        elif self.mechanismID == "B" or self.mechanismID == "B2":
+            self.state_labels = [
+                'HAFR', 'HAFP', 'aHIF', 'HIF1R', 'HIF1P', 
+                'HIF2R', 'HIF2P', 'HIF2P*', 'DSRE2R', 'DSRE2P' 
+            ]
+            # self.topology_gradient = self.topology_gradient_B
+
+        elif self.mechanismID == "C":
+            self.state_labels = [
+                'HAFR', 'HAFP', 'SUMOR', 'SUMOP', 'HAFS', 
+                'aHIF', 'HIF1R', 'HIF1P', 'HIF2R', 'HIF2P',
+                'HIF2P*', 'DSRE2R', 'DSRE2P'
+            ]
+            # self.topology_gradient = self.topology_gradient_C
+
+        elif self.mechanismID == "D" or self.mechanismID == "D2":
+            self.state_labels = [
+                'HAFR', 'HAFP', 'SUMOR', 'SUMOP', 'HAFS', 
+                'aHIF', 'HIF1R', 'HIF1P', 'HIF2R', 'HIF2P', 
+                'HIF2P*', 'DSRE2R', 'DSRE2P'
+            ]
+            self.topology_gradient = self.topology_gradient_D
+
         number_of_states = len(self.state_labels)
         y_init = np.zeros(number_of_states)
+        self.number_of_states = number_of_states
         self.initial_conditions = y_init
 
     def solve_single(
-        self, parameter_labels: List[str]
+        self, topology: str, t_hypoxia: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-        """Solves synTF_Chem model for a single set of parameters and inputs, including 2 steps
-           1) Time from transfection to ligand addition
-           2) Time from ligand addition to measurement via flow cytometry
+        """Solves single HBS topology model for a single set of parameters and inputs,
+        including 2 steps:
+           1) time = 0 hours to time = 500 hours in normoxia (21% O2)
+           2) time = 0 hours to time = 120 hours in hypoxia (1% O2)
 
         Parameters
         ----------
         parameter_labels
             a list of strings defining the parameter labels
 
+        topology
+            a string defining the topology ("simple" for simple HBS, "H1a_fb" for HBS
+            with HIF1a feedback, or "H2a_fb" for HBS with HIF2a feedback)
+
         Returns
         -------
         solution
             An array of ODE solutions (rows are timepoints and columns are model states)
 
-        t
+        t_hypoxia
             A 1D array of time values corresponding to the rows in solution
 
         """
-        # solve before ligand addition
-        timesteps = 100
-        end_time1 = 18
-        tspace_before_ligand_addition = np.linspace(0, end_time1, timesteps)
-        t = tspace_before_ligand_addition
-        solution_before_ligand_addition = odeint(
-            self.gradient,
+        # solve in normoxia
+        solution_normoxia = odeint(
+            self.topology_gradient,
             self.initial_conditions,
-            t,
+            self.t_normoxia,
             args=(
                 self.parameters,
-                self.inputs,
+                self.pO2_normoxia,
                 self.mechanismID,
+                topology
             ),
         )
-
-        # solve after ligand addition
-        for i, label in enumerate(parameter_labels):
-            if label == "e":
-                input_ligand_transformed = self.input_ligand * self.parameters[i]
-
-        end_time2 = 24
-        tspace_after_ligand_addition = np.linspace(0, end_time2, timesteps)
-        t = tspace_after_ligand_addition
-        initial_conditions_after_ligand_addition = np.array(solution_before_ligand_addition[-1, :])
-        initial_conditions_after_ligand_addition[4] = input_ligand_transformed
-        solution_after_ligand_addition = odeint(
-            self.gradient,
-            initial_conditions_after_ligand_addition,
-            t,
+        solution_normoxia_dict = {}
+    
+        for i in range(0,self.number_of_states):
+            solution_normoxia_dict[self.state_labels[i]] = solution_normoxia[:,i]
+        
+        # solve in hypoxia
+        initial_conditions_hypoxia = []
+    
+        for label in self.state_labels:
+            initial_conditions_hypoxia.append(solution_normoxia_dict[label][-1])
+            
+        solution_hypoxia = odeint(
+            self.topology_gradient,
+            initial_conditions_hypoxia,
+            t_hypoxia,
             args=(
                 self.parameters,
-                self.inputs,
+                self.pO2_hypoxia,
                 self.mechanismID,
+                topology
             ),
         )
+        solution_hypoxia_dict = {}
 
-        return (
-            tspace_before_ligand_addition,
-            tspace_after_ligand_addition,
-            solution_before_ligand_addition,
-            solution_after_ligand_addition,
-        )
+        for i in range(0,self.number_of_states):
+            solution_hypoxia_dict[self.state_labels[i]] = solution_hypoxia[:,i]
+        
+        return solution_normoxia_dict, t_hypoxia, solution_hypoxia_dict
 
     @staticmethod
     def topology_gradient_D(
         y: np.ndarray, 
         t: np.ndarray, 
         parameters: list[float], 
-        input: float, 
+        input_pO2: float, 
         mechanismID: str, 
         topology: str
     ) -> np.ndarray:
@@ -152,7 +177,7 @@ class HBS_model:
         parameters
             a list of floats defining the parameters
 
-        input
+        input_pO2
             a float defining the input O2 pressure (pO2)
 
         mechanismID
@@ -169,7 +194,7 @@ class HBS_model:
 
         """
 
-        pO2 = input
+        pO2 = input_pO2
         #mech ID statements
         if mechanismID == 'model_D2':
             ([
@@ -245,82 +270,81 @@ class HBS_model:
             dydt_H2a_fb[8] = k_txn + k_txnBH*(y[7] + y[10]) - k_dR*y[8]
             return dydt_H2a_fb
 
-    def solve_experiment(self, x: list, dataID: str, parameter_labels: List[str]) -> list:
-        """Solve synTF_Chem model for a list of ligand values.
+    def solve_experiment(
+        self, 
+        t_hypoxia: np.ndarray, 
+        topologies: list[str], 
+        dataID: str
+    ) -> dict:
+        """Solve all HBS topology models in normoxia and hypoxia.
 
         Parameters
         ----------
-        x
-            a list of floats containing the independent variable
+        topologies
+            a list of strings defining the topologies to be solved
 
         dataID
             a string defining the dataID
-
-        parameter_labels
-            a list of strings defining the parameter labels
 
         Returns
         -------
         solutions
             A list of floats containing the value of the reporter protein
-            at the final timepoint for each ligand amount
+            at ????
 
         """
+        all_topology_hypoxia_dict = {}
 
-        solutions = []
+        for topology in topologies:
+            _, _, solution_hypoxia_dict = self.solve_single(
+                topology,
+                t_hypoxia,
+            )
+            all_topology_hypoxia_dict[topology] = solution_hypoxia_dict
 
-        if dataID in ("ligand dose response", "ligand dose response and DBD dose response"):
-            self.inputs = [50, 50]  # ng
-            for ligand in x[:11]:
-                self.input_ligand = ligand
-                _, _, _, sol = self.solve_single(parameter_labels)
-                solutions.append(sol[-1, -1])
+        return all_topology_hypoxia_dict
 
-        if dataID == "ligand dose response and DBD dose response":
-            for ad_dose in [20, 10]:  # ng
-                for dbd_dose in x[11:19]:
-                    self.inputs = [dbd_dose, ad_dose]
-                    self.input_ligand = 100
-                    _, _, _, sol = self.solve_single(parameter_labels)
-                    solutions.append(sol[-1, -1])
+    # @staticmethod
+    # def normalize_data(solutions_raw: dict[str, dict[str, float]], dataID: str) -> list[float]:
+    #     """Normalizes data by maximum value
 
-        return solutions
+    #     Parameters
+    #     ----------
+    #     solutions_raw
+    #         a list of floats defining the solutions before normalization
 
-    @staticmethod
-    def normalize_data(solutions_raw: List[float], dataID: str) -> List[float]:
-        """Normalizes data by maximum value
+    #     dataID
+    #         a string defining the dataID
 
-        Parameters
-        ----------
-        solutions_raw
-            a list of floats defining the solutions before normalization
+    #     Returns
+    #     -------
+    #     solutions_norm
+    #         a list of floats defining the dependent variable for the given
+    #         dataset (after normalization)
 
-        dataID
-            a string defining the dataID
+    #     """
+    #     if dataID == 'hypox only':
+    #         DSRed2P_1a = np.append(SS_hox_1a[7.6]['DSRE2P'][:5], SS_hox_1a[138]['DSRE2P'][0])
+    #         DSRed2P_4b = np.append(SS_hox_4b[7.6]['DSRE2P'], SS_hox_4b[138]['DSRE2P'][0])
+    #         DSRed2P_4c = np.append(SS_hox_4c[7.6]['DSRE2P'], SS_hox_4c[138]['DSRE2P'][0])
 
-        Returns
-        -------
-        solutions_norm
-            a list of floats defining the dependent variable for the given
-            dataset (after normalization)
 
-        """
 
-        if dataID == "ligand dose response and DBD dose response":
-            # normalize ligand dose response
-            solutions_norm_1 = [i / max(solutions_raw[:11]) for i in solutions_raw[:11]]
+    #     if dataID == "ligand dose response and DBD dose response":
+    #         # normalize ligand dose response
+    #         solutions_norm_1 = [i / max(solutions_raw[:11]) for i in solutions_raw[:11]]
 
-            # normalize DBD dose response
-            solutions_norm_2 = [i / max(solutions_raw[11:]) for i in solutions_raw[11:]]
+    #         # normalize DBD dose response
+    #         solutions_norm_2 = [i / max(solutions_raw[11:]) for i in solutions_raw[11:]]
 
-            # combine solutions
-            solutions_norm = solutions_norm_1 + solutions_norm_2
+    #         # combine solutions
+    #         solutions_norm = solutions_norm_1 + solutions_norm_2
 
-        elif dataID == "ligand dose response":
-            # normalize ligand dose response
-            solutions_norm = [i / max(solutions_raw) for i in solutions_raw]
+    #     elif dataID == "ligand dose response":
+    #         # normalize ligand dose response
+    #         solutions_norm = [i / max(solutions_raw) for i in solutions_raw]
 
-        return solutions_norm
+    #     return solutions_norm
 
     @staticmethod
     def plot_training_data(
