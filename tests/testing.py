37381577c20b45796f70ff268b1ec7c145accb91
Kathleen Dreyer <kathleendreyer2024@u.northwestern.edu>
2024-01-09 12:23:29 -0500

    add jupyter to poetry project and lock files; run tests of all stats functions in testing

--- tests/testing.py
+++ tests/testing.py
@@ -4,8 +4,10 @@ import seaborn as sns
 import statsmodels.api as sm 
 import csv
 from pingouin import read_dataset
-from games.experimental_statistical_tests import (anova,
+from games.modules.experimental_statistical_testing.experimental_statistical_tests import (anova,
                                                   tukeys_hsd,
+                                                  t_tests,
+                                                  BH_correction,
                                                   run_corrected_t_tests,
                                                   repeated_measures_anova)
                                                   
@@ -42,12 +44,46 @@ from games.experimental_statistical_tests import (anova,
 # print(tukeys_table)
 
 # two-way ANOVA and tukey's HSD
-df = pd.read_csv("/Users/kdreyer/Desktop/ToothGrowth.csv")
-df["dose"] = df["dose"].astype(str)
+# df = pd.read_csv("/Users/kdreyer/Desktop/ToothGrowth.csv")
+# df["dose"] = df["dose"].astype(str)
+# # print(df)
+# anova_table = anova("", "", df, "len", "supp", "dose")
+# print(anova_table)
+# tukeys_table_i, tukeys_tables = tukeys_hsd("", "", "len", df, ["supp", "dose"], ["supp", "dose"])
+# print(tukeys_table_i)
+# for table in tukeys_tables:
+#     print(table)
+
+# t-tests with benjamini-hochberg correction
+# df = pd.read_csv("/Users/kdreyer/Desktop/iris.csv")
+# # print(df)
+# d_vars = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+# full_t_tests = pd.DataFrame(columns = ["dep_var", 'p_value'])
+# full_t_tests["dep_var"] = d_vars
+# p_vals = []
+# for var in d_vars:
+#     table = t_tests(var, df, [["versicolor", "virginica"]])
+#     p_vals.append(table["p_value"].to_list()[0])
+# full_t_tests["p_value"] = p_vals
+# # print(full_t_tests)
+
+# corrected_vals = BH_correction(full_t_tests)
+# print(corrected_vals)
+
+# repeated measures ANOVA
+# df = pd.read_csv("/Users/kdreyer/Desktop/rm_anova.csv")
+# print(type(df.at[60, "sleep"]))
+# rm_anova = repeated_measures_anova(path="", fig_name="", data_df=df, dep_var="sleep", within_var="day", between_var="condition")
+# print(rm_anova)
+# df = read_dataset('rm_anova_wide')
+# print(df.head(n=15))
+# df["subject"] = df["Subject"]
+# print(type(df.at[0, "Scores"]))
+# rm_anova = repeated_measures_anova("", "", df, "Scores", "Time", "Group")
+# print(rm_anova)
+
+df = pd.read_csv("/Users/kdreyer/Desktop/Exp10_H1a_fb_HBS.csv")
+df = df[df["day"]!= "normoxic"]
 # print(df)
-anova_table = anova("", "", df, "len", "supp", "dose")
-print(anova_table)
-tukeys_table_i, tukeys_tables = tukeys_hsd("", "", "len", df, ["supp", "dose"], ["supp", "dose"])
-print(tukeys_table_i)
-for table in tukeys_tables:
-    print(table)
+rm_anova = repeated_measures_anova("", "", df, "meptrs", "day")
+print(rm_anova)
