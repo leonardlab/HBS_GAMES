7575c8fccb161db8708957e644cf004ba9e80e44
Kathleen Dreyer <kathleendreyer2024@u.northwestern.edu>
2024-01-09 12:58:34 -0500

    update experimental_statistical_tests import location, remove outdated info, and fix groups arg in tukeys_hsd in testing

--- tests/testing.py
+++ tests/testing.py
@@ -4,44 +4,24 @@ import seaborn as sns
 import statsmodels.api as sm 
 import csv
 from pingouin import read_dataset
-from games.modules.experimental_statistical_testing.experimental_statistical_tests import (anova,
+from games.modules.experimental_statistiical_testing.experimental_statistical_tests import (anova,
                                                   tukeys_hsd,
                                                   t_tests,
                                                   BH_correction,
                                                   run_corrected_t_tests,
                                                   repeated_measures_anova)
                                                   
-
-
-
-# Ensure reproducibility of the results
-# np.random.seed(123)
-
-# # Create three normally-distributed vectors.
-# n = 20
-# perf2010 = np.random.normal(loc=5, scale=0.8, size=n)     # Mean = 5, standard deviation = 0.8, size = 20
-# perf2014 = np.random.normal(loc=4.8, scale=0.8, size=n)   # Mean = 4.8, ...
-# perf2018 = np.random.normal(loc=3, scale=0.8, size=n)     # Mean = 3, ...
-
-# # Concatenate in a long-format pandas DataFrame
-# df = pd.DataFrame({'Memory': np.r_[perf2010, perf2014, perf2018],
-#                    'Year': np.repeat(['2010', '2014', '2018'], n),
-#                    'Subject': np.tile(np.arange(n), 3)})
-
-# df['Sex'] = np.tile(np.repeat(['Men', 'Women'], 10), 3)
-# print(df)
-
-
 # one-way ANOVA and tukey's HSD
-# df = pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/difficile.csv")
-# df.drop('person', axis= 1, inplace= True)
-# # Recoding value from numeric to string
-# df['dose'].replace({1: 'placebo', 2: 'low', 3: 'high'}, inplace= True)
-# # print(df)
-# anova_table = anova("", "", df, "libido", "dose", None)
-# # print(anova_table)
-# tukeys_table = tukeys_hsd("", "", "libido", df, "dose")
-# print(tukeys_table)
+df = pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/difficile.csv")
+df.drop('person', axis= 1, inplace= True)
+# Recoding value from numeric to string
+df['dose'].replace({1: 'placebo', 2: 'low', 3: 'high'}, inplace= True)
+# print(df)
+anova_table = anova("", "", df, "libido", "dose", None)
+# print(anova_table)
+tukeys_tables = tukeys_hsd("", "", "libido", df, ["dose"])
+for table in tukeys_tables:
+    print(table)
 
 # two-way ANOVA and tukey's HSD
 # df = pd.read_csv("/Users/kdreyer/Desktop/ToothGrowth.csv")
@@ -71,19 +51,8 @@ from games.modules.experimental_statistical_testing.experimental_statistical_tes
 # print(corrected_vals)
 
 # repeated measures ANOVA
-# df = pd.read_csv("/Users/kdreyer/Desktop/rm_anova.csv")
-# print(type(df.at[60, "sleep"]))
-# rm_anova = repeated_measures_anova(path="", fig_name="", data_df=df, dep_var="sleep", within_var="day", between_var="condition")
+# df = pd.read_csv("/Users/kdreyer/Desktop/Exp10_H1a_fb_HBS.csv")
+# df = df[df["day"]!= "normoxic"]
+# # print(df)
+# rm_anova = repeated_measures_anova("", "", df, "meptrs", "day")
 # print(rm_anova)
-# df = read_dataset('rm_anova_wide')
-# print(df.head(n=15))
-# df["subject"] = df["Subject"]
-# print(type(df.at[0, "Scores"]))
-# rm_anova = repeated_measures_anova("", "", df, "Scores", "Time", "Group")
-# print(rm_anova)
-
-df = pd.read_csv("/Users/kdreyer/Desktop/Exp10_H1a_fb_HBS.csv")
-df = df[df["day"]!= "normoxic"]
-# print(df)
-rm_anova = repeated_measures_anova("", "", df, "meptrs", "day")
-print(rm_anova)
