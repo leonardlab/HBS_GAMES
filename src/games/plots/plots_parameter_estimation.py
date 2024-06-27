1428e6b6421838132f6ab4640622557687e05855
Kathleen (Katie) Dreyer <kathleendreyer2024@u.northwestern.edu>
2024-06-27 15:24:18 -0500

    minor updates to readme and docstrings; remove outdated plotting function

--- src/games/plots/plots_parameter_estimation.py
+++ src/games/plots/plots_parameter_estimation.py
@@ -40,66 +40,6 @@ def plot_chi_sq_trajectory(chi_sq_list: List[float]) -> None:
     plt.savefig("chi_sq trajectory for best fit.svg", dpi=600)
 
 
-def plot_training_data_fits(df_opt: pd.DataFrame) -> None:
-    """Plots fits to training data with similarly good r_sq values
-
-    Note: This function is not currently generalizable to other models and datasets and the
-     user must modify before running
-
-    Parameters
-    ----------
-    df_opt
-        a dataframe containing the parameter estimation results after optimization
-
-    Returns
-    -------
-    None
-
-    """
-
-    # Only keep rows for which r_sq >= .99
-    df_opt = df_opt[df_opt["r_sq"] >= 0.99]
-    dose_responses = list(df_opt["Simulation results"])
-
-    # define experimental data
-    x = df_opt["x"].iloc[0]
-    exp_data = df_opt["exp_data"].iloc[0]
-    exp_error = df_opt["exp_error"].iloc[0]
-
-    plt.figure(figsize=(3, 3))
-    ax1 = plt.subplot(111)
-
-    # Plot experimental/training data
-    ax1.errorbar(
-        x,
-        exp_data,
-        color="black",
-        marker="o",
-        yerr=exp_error,
-        fillstyle="none",
-        linestyle="none",
-        capsize=2,
-        label="Training data",
-    )
-    ax1.set_xscale("symlog")
-
-    # Plot simulated data for each parameter set in df_opt
-    sns.set_palette("Greys", len(dose_responses))
-    count = 0
-    for dose_response in dose_responses:
-        count += 1
-        ax1.plot(
-            x,
-            dose_response,
-            linestyle=":",
-            marker=None,
-            label="Model fit " + str(count),
-        )
-    ax1.set_xlabel("Ligand dose (nM)")
-    ax1.set_ylabel("Reporter expression")
-    plt.savefig("./fits r_sq above 0.99.svg", bbox_inches="tight")
-
-
 def plot_parameter_distributions_after_optimization(
     df_opt: pd.DataFrame, parameter_labels: List[str]
 ) -> None:
