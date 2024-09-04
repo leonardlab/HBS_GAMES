41c9ac99bb1f09b5564a3b5cc1875238667a2013
Kathleen (Katie) Dreyer <kathleendreyer2024@u.northwestern.edu>
2024-09-04 13:17:35 -0500

    update plot styles for figures 1-4

--- src/games/paper.mplstyle.py
+++ src/games/paper.mplstyle.py
@@ -4,17 +4,22 @@ axes.spines.right: False
 axes.labelpad: 10
 
 #lines and markers
-lines.linewidth: 2
-lines.markersize: 6
+lines.linewidth: 0.5
+lines.markersize: 5
 
 #fonts, labels, and titles
 font.sans-serif: Arial
-axes.titlesize: 12
+axes.titlesize: 6
 #font.weight:  bold
 #axes.labelweight : bold
-axes.labelsize: 9
-xtick.labelsize: 9
-ytick.labelsize: 9
+axes.labelsize: 6
+axes.linewidth: 0.5
+xtick.labelsize: 6
+xtick.major.width: 0.5
+xtick.minor.width: 0.5
+ytick.labelsize: 6
+ytick.major.width: 0.5
+ytick.minor.width: 0.5
 
 #Saving
 savefig.dpi: 600
